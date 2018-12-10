9b60aaff48bb69eaad6b7b2c60b0d2a551a78def
tcgwl <tcgwl_123@126.com>
2018-12-10 18:26:50 +0800

    添加isUserModule

---
+++ BaseLibrary/src/main/java/com/kotlin/base/common/BaseApplication.java
@@ -0,0 +1,11 @@
+package com.kotlin.base.common;
+
+import android.app.Application;
+
+public class BaseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
